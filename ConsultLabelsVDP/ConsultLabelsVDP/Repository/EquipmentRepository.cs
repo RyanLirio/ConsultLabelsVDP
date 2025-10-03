6a017f96a8d17a233bf049098689c79c7701480a
Ryan Pablo Krauchuk Lirio <ryan.lirio@videplast.com.br>
2025-10-03 16:55:43 -0300

    Criando o Repository dos equipamentos

---
+++ ConsultLabelsVDP/ConsultLabelsVDP/Repository/EquipmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using ConsultLabelsVDP.Models;
+
+namespace ConsultLabelsVDP.Repository
+{
+    public class EquipmentRepository
+    {
+        private static readonly List<Equipment> _equipments = new List<Equipment>
+        {
+            new Equipment { Id = 1, Codigo = "EQ001", Nome = "Laptop", Fornecedor = "Dell", DataCompra = new DateTime(2022, 1, 15), Situacao = "Ativo" }
+        };
+
+        public async Task AddEquipment(Equipment equipment)
+        {
+
+        }
+
+        public async Task<Equipment> GetByCodigo(string Codigo)
+        {
+            
+        }
+    }
+}
---
+++ ConsultLabelsVDP/ConsultLabelsVDP/Repository/IEquipmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using ConsultLabelsVDP.Models;
+
+namespace ConsultLabelsVDP.Repository
+{
+    public interface IEquipmentRepository
+    {
+        public Task AddEquipment(Equipment equipment);
+        public Task<Equipment> GetByCodigo (string Codigo);
+    }
+}
