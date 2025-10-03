e333acafde4a4b417d274812f56f850322411f9a
Ryan Pablo Krauchuk Lirio <ryan.lirio@videplast.com.br>
2025-10-03 16:19:43 -0300

    Iniciando o projeto e criado o model Equipment

---
+++ ConsultLabelsVDP/ConsultLabelsVDP/Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsultLabelsVDP.Models
+{
+    public class Equipment
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Codigo { get; set; }
+        public string Nome { get; set; }    
+        public string Fornecedor { get; set; }
+        public DateTime DataCompra { get; set; }
+        public string Situacao { get; set; }
+    }
+}
